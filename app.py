2866f554b1cb31e6d8a35e5633cac86bebe9b03e
RoachLok <jtabordajunior@hotmail.com>
2022-07-02 08:14:08 +0200

    Implement API endpoint for shots_pivot_results().

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, make_response, request, jsonify
 from flask_caching import Cache
 
 from util.databridge import Databridge
-from transform.penalty_cards_agg import penalty_cards_agg
+from transform.jobs import penalty_cards_agg, shots_pivot_results
 
 import schema.data_structs as schema
 import data.paths as data_routes
@@ -22,7 +22,9 @@ dfs_info = [
     (data_routes.trmkt_competitions , schema.trmkt_competitions , 'trmkt_competitions'  ),
     (data_routes.trmkt_games        , schema.trmkt_games        , 'trmkt_games'         ),
     (data_routes.trmkt_leagues      , schema.trmkt_leagues      , 'trmkt_legues'        ),
-    (data_routes.trmkt_players      , schema.trmkt_players      , 'trmkt_players'       )
+    (data_routes.trmkt_players      , schema.trmkt_players      , 'trmkt_players'       ),
+    (data_routes.tech_players       , schema.tech_players       , 'tech_players'        ),
+    (data_routes.tech_shots         , schema.tech_shots         , 'tech_shots'          )
 ]
 
 data.add_dataframes([(reader.csv(path, header=True, schema=schema), id) for path, schema, id in dfs_info])
@@ -40,9 +42,10 @@ def index():
 @app.route('/api/penalty_cards')
 @cache.cached(timeout=300, query_string=True)
 def cards():
-    order_by = request.args.get('order_by') or 'yellow_cards'
-    player_positon = request.args.get('position') or None
-    format_csv = request.args.get('json') == 'false'
+    order_by        = request.args.get('order_by') or 'yellow_cards'
+    player_positon  = request.args.get('position')
+    format_csv      = request.args.get('json'    ) 
+
 
     if not player_positon:
         return '{"response": "Bad Request"}', 400, {'Content-type' : 'application/json'}
@@ -54,7 +57,7 @@ def cards():
         sums_keys       = ['yellow_cards', 'red_cards'],
         pl_position     = player_positon,
         order_by        = order_by,
-        csv = format_csv
+        csv = format_csv == 'false' if format_csv else True
     )
 
 
@@ -63,24 +66,45 @@ def cards():
     return make_response(response_str, 200, response_meta) if format_csv \
         else response_str, 200, {'Content-type' : 'application/json'}
 
-
-
-"""
-Sample query handling
-
-@app.route('/expenditure')
+@app.route('/api/shots')
 @cache.cached(timeout=300, query_string=True)
-def expenditure():
-    query_by = request.args.get('query_by') or 'by_day'
-    sector_filter = request.args.get('sector_filter') or None
+def shots():
+    situation       = request.args.get('situation'    )
+    order_by        = request.args.get('order_by'     ) or 'Goal'
+    player_names    = request.args.get('player_names' )
+    only_goalers    = request.args.get('only_goalers' )
+    percentage      = request.args.get('percentage'   )
+    format_csv      = request.args.get('json'         ) 
+    
+    player_names    = player_names   == 'true' if player_names  else False
+    only_goalers    = only_goalers   == 'true' if only_goalers  else False
+    percentage      = percentage     == 'true' if percentage    else False
+    
+    format_csv = format_csv == 'false' if format_csv else True
 
-    csv_str = expense.query(data, modifier=query_by, sector_filter=sector_filter)
+    if not situation:
+        return '{"response": "Bad Request"}', 400, {'Content-type' : 'application/json'}
 
-    if not csv_str:
-        return make_response('{"message": "Bad filter"}', 400, {'Content-type' : 'application/json'})
+    names_df = None 
+    if player_names:
+        names_df = data.get_dataframe('tech_players')
 
-    return make_response(csv_str, 200, {'Content-Disposition' : 'attachment; filename=export.csv', 'Content-type' : 'text/csv'})
-"""
+
+    response_str = shots_pivot_results(
+        shots_df        = data.get_dataframe('tech_shots'),
+        situation       = situation,
+        players_df      = names_df,
+        order_by        = order_by,
+        only_goalers    = only_goalers,
+        percentage      = percentage,
+        csv = format_csv
+    )
+
+
+    response_meta = {'Content-Disposition' : 'attachment; filename=export.csv', 'Content-type' : 'text/csv'}
+
+    return make_response(response_str, 200, response_meta) if format_csv \
+        else response_str, 200, {'Content-type' : 'application/json'}
 
 
 if __name__ == '__main__':
